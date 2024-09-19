f5a8174059d0849b2968bb5412857ba84010e2db
Anderson Souza <andersonssilveira96@gmail.com>
2024-09-18 21:46:41 -0300

    Adding metrics server

--- src/kubernetes/setup.ps1
+++ src/kubernetes/setup.ps1
@@ -1,7 +1,7 @@
 minikube start
 
 # Metrics para obter informações e fazer o HPA da API.
-kubectl apply -f metrics-server.yaml
+kubectl apply -f ./src/kubernetes/metrics-server.yaml
 
 # API
 kubectl apply -f ./src/kubernetes/api-secret.yaml
